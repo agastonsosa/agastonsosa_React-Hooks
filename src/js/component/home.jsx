9ec6211e456532ac104c54a8a9f75705b3f3cd9d
agastonsosa <138701662+agastonsosa@users.noreply.github.com>
2024-02-27 15:52:43 +0000

    dom example ok

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,1 +1,21 @@
+import React, { useState, useEffect } from 'react';
 
+const Home = () => {
+  const [contador, setContador] = useState(0);
+
+  // Usamos useEffect para actualizar el título del documento
+  useEffect(() => {
+    document.title = `Has hecho click ${contador} veces`;
+  }, [contador]); // Este array de dependencias hace que el efecto se ejecute solo cuando "contador" cambia.
+
+  return (
+    <div>
+      <p>Has hecho click {contador} veces</p>
+      <button onClick={() => setContador(contador + 1)}>
+        Hazme click
+      </button>
+    </div>
+  );
+}
+
+export default Home
